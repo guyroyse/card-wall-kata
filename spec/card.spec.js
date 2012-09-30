ce73bc20de8cc4f7ec5434e0090b6073b4104da5
Guy Royse <guy@guyroyse.com>
2012-09-30 12:45:10 -0400

    refactored tests to include a little more grouping

--- spec/card.spec.js
+++ spec/card.spec.js
@@ -5,90 +5,113 @@ describe("Card", function() {
     card = createCard("name", "desc", 1);
   });
 
-  it("has a name", function(){
-    expect(card.name).toBe("name");
-  });
+  describe("when created", function() {
 
-  it("has a description", function(){
-    expect(card.description).toBe("desc");
-  });
+    it("has a name", function(){
+      expect(card.name).toBe("name");
+    });
 
-  it("has a priority", function(){
-    expect(card.priority).toBe(1);
-  });
+    it("has a description", function(){
+      expect(card.description).toBe("desc");
+    });
 
-  it("has a done default of false", function(){
-    expect(card.done).toBe(false)
-  });
+    it("has a priority", function(){
+      expect(card.priority).toBe(1);
+    });
 
-  it("has a start date that is null",function(){
-    expect(card.startDate).toBe(null);
-  });
+    it("has a complexity", function() {
+      expect(card.complexity).toBeDefined();
+    });
+
+    it("is not done", function(){
+      expect(card.done).toBe(false)
+    });
+
+    it("is not blocked",function(){
+      expect(card).not.toBeBlocked();
+    });
+
+    it("has no start date",function(){
+      expect(card.startDate).toBe(null);
+    });
   
-  it("has a start date when started", function() {
-    var before = Date.now();
-    card.start();
-    var after = Date.now();
+    it("has no end date",function(){
+      expect(card.endDate).toBe(null);
+    });
 
-    expect(card.startDate.getTime()).toBeBetween(before, after);
+    it("has no assignees", function() {
+      expect(card.assignees).toEqual([])
+    });
+
   });
 
-  it("has an end date that is null",function(){
-    expect(card.endDate).toBe(null);
+  describe("when started", function() {
+
+    it("has a start date when started", function() {
+      var before = Date.now();
+      card.start();
+      var after = Date.now();
+
+      expect(card.startDate.getTime()).toBeBetween(before, after);
+    });
+
   });
 
-  it("can be completed", function() {
-    card.complete();
-    expect(card.done).toBe(true);
+  describe("when completed", function() {
+
+    it("can be completed", function() {
+      card.complete();
+      expect(card.done).toBe(true);
+    });
+
+    it("has an end date when completed", function() {
+      var before = Date.now();
+      card.complete();
+      var after = Date.now();
+
+      expect(card.endDate.getTime()).toBeBetween(before, after);
+    });
+
   });
 
-  it("has an end date when completed", function() {
-    var before = Date.now();
-    card.complete();
-    var after = Date.now();
+  describe("when adding assignees", function() {
 
-    expect(card.endDate.getTime()).toBeBetween(before, after);
-  });
-
-  it("has no assignee", function(){
-    expect(card.assignees).toEqual([])
-  });
-
-  it("has a blocked property starts as false",function(){
-    expect(card).not.toBeBlocked();
-  });
-
-  it("can be blocked",function(){
-    card.block();
-    expect(card).toBeBlocked();
-  });
-
-  it("can be unblocked",function(){
-    card.unblock();
-    expect(card).not.toBeBlocked();
-  });
-
-  it("has one assignee", function() {
-    card.addPerson("name");
-    expect(card.assignees.length).toBe(1);
-  });
-
-  it("has two assignees", function(){
+    it("accepts assignees", function() {
       card.addPerson("name");
-      card.addPerson("name2");
-      expect(card.assignees.length).toBe(2);
+      expect(card.assignees.length).toBe(1);
+    });
+
+    it("accepts multiple assignees", function(){
+        card.addPerson("name");
+        card.addPerson("name2");
+        expect(card.assignees.length).toBe(2);
+    });
+
+    it("accepts only unique assignees", function(){
+
+        card.addPerson("name");
+        card.addPerson("name2");
+        card.addPerson("name2");
+        expect(card.assignees.length).toBe(2);
+    });
+
   });
 
-  it("has unique assignees", function(){
-      card.addPerson("name");
-      card.addPerson("name2");
-      card.addPerson("name2");
-      expect(card.assignees.length).toBe(2);
-  });
+  describe("when blocked", function() {
 
-  it("has complexity property", function(){
-    var card = createCard("name","desc",4)
-    expect(card.complexity).toBeDefined();
+    beforeEach(function() {
+      card.block();
+    });
+
+    it("is is blocked",function(){
+      expect(card).toBeBlocked();
+    });
+
+    it("can be unblocked",function(){
+      card.unblock();
+      expect(card).not.toBeBlocked();
+    });
+
   });
 
 });
